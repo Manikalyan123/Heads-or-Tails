5f6a05f904772b3cb3d7ab9bc52e561051fbf6e7
Manikalyan123 <kalyan.boddepalli1999@gmail.com>
2022-12-15 12:57:06 +0000

    solution

---
+++ src/components/CoinToss/index.js
@@ -0,0 +1,48 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class CoinToss extends Component {
+  state = {
+    Heads: 0,
+    Tails: 0,
+    image: 'https://assets.ccbp.in/frontend/react-js/heads-img.png',
+  }
+
+  onToss = () => {
+    const Toss = Math.ceil(Math.random() * 10)
+    if (Toss > 5) {
+      this.setState(prevState => ({
+        Heads: prevState.Heads + 1,
+        image: 'https://assets.ccbp.in/frontend/react-js/heads-img.png',
+      }))
+    } else if (Toss <= 5) {
+      this.setState(prevState => ({
+        Tails: prevState.Tails + 1,
+        image: 'https://assets.ccbp.in/frontend/react-js/tails-img.png',
+      }))
+    }
+  }
+
+  render() {
+    const {Heads, Tails, image} = this.state
+    return (
+      <div className="Cont">
+        <div className="content-cont">
+          <h1 className="heading">Coin Toss Game</h1>
+          <p className="passage">Heads (or) Tails</p>
+          <img className="image" src={image} alt="toss result" />
+          <button className="button" type="button" onClick={this.onToss}>
+            Toss Coin
+          </button>
+          <div className="count-cont">
+            <p>Total:{Heads + Tails}</p>
+            <p>Heads:{Heads}</p>
+            <p>Tails:{Tails}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default CoinToss
